bf27d5652cb03be6899a760a7a85bd7c56d5f39d
Anna Doubkova <anna.doubkova@red-badger.com>
2017-02-02 17:26:44 +0000

    Add options to command; extend table connection

--- .serverless_plugins/plugin.js
+++ .serverless_plugins/plugin.js
@@ -1,32 +1,99 @@
-import aws from 'aws-sdk';
+const aws = require('aws-sdk');
 
-const downloadData = () => {
-  aws.scan(serverless.tableName, () => {
-    // put the data somewhere
+const downloadData = (serverless, options) => {
+  const { Resources } = serverless.service.resources;
+  const { resource } = options;
+  console.log(options);
+  console.log('aws', Resources[resource].Properties.TableName);
+  // console.log('variables', serverless.variables);
+  // console.log('config', serverless.config);
+  return;
+
+  AWS.config.update({
+    region: serverless.service.provider.region,
+    apiVersions: {
+      dynamodb: '2012-08-10',
+    }
+  });
+  const dynamodb = new AWS.DynamoDB();
+
+  const params = {
+    tableName: serverless.tableName
+  };
+
+  dynamodb.scan(params, (err, result) => {
+    if (err) {
+      serverless.cli('Oh no down! ', err);
+    }
+    serverless.data = result;
+    serverless.cli('All good down! ', result);
   });
 };
 
 const uploadData = () => {
-  //pick up the data from somewhere
-  data.forEach(d => {
-    aws.putItem(serverless.tableName, () => {});
+  console.log('upload');
+  return;
+
+  AWS.config.update({
+    region: "eu-west-1",
+    apiVersions: {
+      dynamodb: '2012-08-10',
+    }
+  });
+  const dynamodb = new AWS.DynamoDB();
+
+  serverless.data.forEach(d => {
+    const params = {
+      tableName: serverless.tableName,
+      Key: {
+       'Name': {
+         S: d.Name
+        },
+       'EmailAddress': {
+         S: d.EmailAddress
+        }
+      }
+    };
+    dynamodb.putItem(params, (err, result) => {
+      if (err) {
+        serverless.cli('Oh no up! ', err);
+      }
+      serverless.cli('All good up! ', result);
+    });
   });
 };
 
 class MyPlugin {
-  constructor() {
+  constructor(serverless, options) {
     this.commands = {
       backup: {
         lifecycleEvents: [
           'downloadData',
           'uploadData'
-        ]
+        ],
+        usage: 'Pushes data from one database to another',
+        options: {
+          resource: {
+            usage: 'Specify name of resource for your table',
+            required: true
+          },
+          stage: {
+            usage: 'Stage you want to get data from',
+            required: true,
+            shortcut: 's'
+          },
+          backup: {
+            usage: 'Stage you want to get data from',
+            required: true,
+            shortcut: 'b'
+          }
+        }
       },
     };
 
     this.hooks = {
-      'backup:downloadData': downloadData,
-      'backup:uploadData': uploadData,
+      'backup:downloadData': downloadData.bind(null, serverless, options),
+      'backup:uploadData': uploadData.bind(null, serverless, options),
     };
   }
 }
