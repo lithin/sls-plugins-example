425b7db250d7cf986350c366926be0101d7bc19a
Anna Doubkova <anna.doubkova@red-badger.com>
2017-02-06 14:00:30 +0000

    Code clean-up

--- handler.js
+++ handler.js
@@ -1,9 +1,9 @@
 'use strict';
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 
 module.exports.getUsers = (event, context, callback) => {
   AWS.config.update({
-    region: "eu-west-1",
+    region: 'eu-west-1',
     apiVersions: {
       dynamodb: '2012-08-10',
     }
@@ -16,7 +16,7 @@ module.exports.getUsers = (event, context, callback) => {
 
   dynamodb.scan(params, function(err, data) {
     if (err) {
-      callback("Unable to get items. Error JSON: " + JSON.stringify(err));
+      callback(`Unable to get items. Error JSON: ${JSON.stringify(err)}`);
     } else {
       callback(null, data);
     }
@@ -25,7 +25,7 @@ module.exports.getUsers = (event, context, callback) => {
 
 module.exports.createUser = (event, context, callback) => {
   AWS.config.update({
-    region: "eu-west-1",
+    region: 'eu-west-1',
     apiVersions: {
       dynamodb: '2012-08-10',
     }
@@ -46,9 +46,9 @@ module.exports.createUser = (event, context, callback) => {
 
   dynamodb.updateItem(params, function(err, data) {
     if (err) {
-      callback("Unable to add item. Error JSON: " + JSON.stringify(err));
+      callback(`Unable to add item. Error JSON: ${JSON.stringify(err)}`);
     } else {
-      callback(null, "Added item: " + JSON.stringify(data));
+      callback(null, `Added item: ${JSON.stringify(data)}`);
     }
   });
 };
